c05b72afdafa43965ae57a0985065202d9d9354e
Patrick Mautari <patrick.mautari@gmail.com>
2022-11-07 19:02:32 -0300

    subscribe no loop

--- IoT-Platform/mqtt.hpp
+++ IoT-Platform/mqtt.hpp
@@ -30,7 +30,30 @@ class Mqtt {
 
         void loopMqtt() {
             MQTT.loop();
-            
+            if(!MQTT.connected()){ //Se nao estiver conectado
+                if(reconnect< millis()){ //Executa a solicitacao de conexao a cada 5 segundos
+                Serial.println("");
+                Serial.println("Conectando ao servidor...");
+                //Solicita a conexao com o servidor utilizando os parametros "ID_DISPOSITIVO", "TOKEN_API" e "SENHA_ID" 
+                if(!MQTT.connect(deviceId, tokenApi, mqttPassword)){ 
+                    Serial.println("Falha na conexao com o servidor.");
+                } else {
+                    Serial.println("Conectado!");
+                    Serial.print("Estabelecendo inscricao com ");
+                    Serial.println(topic);
+                    //Solicita a inscricao no "TOPICO"
+                    if(!MQTT.subscribe(topic)){
+                    Serial.println("Erro ao se inscrever no topico");
+                    Serial.println("Desconectando do servidor...");
+                    MQTT.disconnect(); //Desconecta a placa do servidor
+                    Serial.println("Desconectado.");
+                    } else {
+                    Serial.println("Inscricao concluida!");
+                    }
+                }
+                reconnect = millis() + 5000; //Atualiza a contagem de tempo
+                }
+            }
         }
 
         void connectMQTT(const char* ssid, const char* password, const char* topic, const char* mqttHost, int socket, const char* mqttPass) {
@@ -47,9 +70,9 @@ class Mqtt {
             this->mqttPassword = mqttPass;
             this->topic = topic;
             delay(1000);
-            Serial.println("antes do publish");
-            MQTT.publish("conexao", this->deviceId);
-            Serial.println("depois do publish");
+            //Serial.println("antes do publish");
+            //MQTT.publish("conexao", this->deviceId);
+            //Serial.println("depois do publish");
         }
 
         void subscribe() {
